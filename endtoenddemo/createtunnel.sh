60b2bbb3794a476a571db021d9a9744055ab5185
James Clarke <james@clarkezone.net>
2023-04-22 00:06:46 +0000

    Conditional tunnel creation only if not exists

--- endtoenddemo/createtunnel.sh
+++ endtoenddemo/createtunnel.sh
@@ -3,10 +3,15 @@ set -e
 export tunnelname="pocketshortene2edemo-tunnel-prod"
 export domain="psdemo.clarkezone.dev"
 export k8sroot=""
-cloudflared tunnel create $tunnelname
-cloudflared tunnel route dns $tunnelname $domain
+tunnelid=$(cloudflared tunnel list | sed -n "/^.*${tunnelname}[[:space:]].*$/p" | awk '{print $1}')
+if [ -z "$tunnelid" ];
+then
+  echo "Tunnel not found, creating"
+  cloudflared tunnel create $tunnelname
+  cloudflared tunnel route dns $tunnelname $domain
+fi
 tunnelid=$(cloudflared tunnel list | sed -n "/^.*${tunnelname}[[:space:]].*$/p" | awk '{print $1}')
 echo "$tunnelid"
-cp "$HOME/.cloudflared/$tunnelid.json" manifests/layered_viper_apply/manifests/layered_viper_apply/overlay/prod/secrets/.
-cp "$HOME/.cloudflared/cert.pem" manifests/layered_viper_apply/manifests/layered_viper_apply/overlay/prod/secrets/.
+cp "$HOME/.cloudflared/$tunnelid.json" manifests/layered_viper_apply/overlay/prod/secrets/.
+cp "$HOME/.cloudflared/cert.pem" manifests/layered_viper_apply/overlay/prod/secrets/.
 sed -i 's/TUNNEL_NAME/'"$tunnelname"'/; s/TUNNEL_ID/'"$tunnelid"'/; s/HOSTNAME/'"$domain"'/' manifests/layered_viper_apply/overlay/prod/config/config.yaml
