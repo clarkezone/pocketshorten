559799e23ec6f234399077f072184c5847a8c915
James Clarke <james@clarkezone.net>
2023-09-19 07:19:04 -0700

    Show links in config

--- scripts/configadmin/findlinks.py
+++ scripts/configadmin/findlinks.py
@@ -1,24 +1,36 @@
 import re
 import argparse
+import json
 
-def extract_urls(file_path):
+def extract_urls(file_path, json_data):
+    # Create a dictionary with expanded URLs as keys and their corresponding shortened forms as values
+    url_dict = {entry[1]: entry[0] for entry in json_data["values"]}
+
     with open(file_path, 'r') as file:
         content = file.read()
-        pattern = r'\[.*?\]\((http[s]?://(?!q6o\.to).*?)\)'
+        pattern = r'\[.*?\]\((http[s]?://(?!q6o\.io).*?)\)'
         urls = re.findall(pattern, content)
+        print("https://tailscale.com" in url_dict)
         for url in urls:
-            print(url)
-        print("done")
+            if url in url_dict:
+                print(url, "yes, shortened form:", url_dict[url])
+            else:
+                print(url, "no")
 
 def main():
-    parser = argparse.ArgumentParser(description='Extract q6o.com URLs from a markdown file.')
+    parser = argparse.ArgumentParser(description='Check URLs from a markdown file against a JSON list.')
     parser.add_argument('md_file', help='Path to the markdown file.')
+    parser.add_argument('json_file', help='Path to the JSON file containing URLs.')
     args = parser.parse_args()
 
+    # Load the JSON data
+    with open(args.json_file, 'r') as jfile:
+        json_data = json.load(jfile)
+
     try:
-        extract_urls(args.md_file)
+        extract_urls(args.md_file, json_data)
     except FileNotFoundError:
-        print("Error: The provided markdown file was not found.")
+        print("Error: One of the provided files was not found.")
     except Exception as e:
         print(f"An error occurred: {e}")
 
